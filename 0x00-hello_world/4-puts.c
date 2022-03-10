0a91936318a40e57ee20dd2fc0a6baf33fbb6b06
Jaybeegarang <jbarasa57@yahoo.com>
2022-03-10 11:28:27 -0800

    changes to task 4 betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,18 @@
 #include<stdio.h>
 #include<string.h>
+
 /**
  *program to print out string using puts function
+ *
  */
+
 int main(void)
 {
 	char quote[] = "\"Programming is like building a multilingual puzzle\n";
+
 	puts(quote);
-	return 0;
+	/**
+	 *print out value of quote using put
+	 */
+	return (0);
 }
