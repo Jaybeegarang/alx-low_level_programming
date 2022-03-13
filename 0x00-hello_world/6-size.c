5e2a64eb952fd611193d2a9a2c772c723e021b23
Jaybeegarang <jbarasa57@yahoo.com>
2022-03-13 04:26:53 -0700

    Newline character

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,9 +12,9 @@ int main(void)
 	long long int ill;
 	float f;
 
-	printf("Size of a char: %lu bytes(s)", (unsigned long)sizeof(c));
-	printf("Size of an int: %lu bytes(s)", (unsigned long)sizeof(i));
-	printf("Size of a long int: %lu bytes(s)", (unsigned long)sizeof(li));
-	printf("Size of a long long int: %lu bytes(s)", (unsigned long)sizeof(ill));
-	printf("Size of a float: %lu bytes(s)", (unsigned long)sizeof(f));
+	printf("Size of a char: %lu bytes(s)\n", (unsigned long)sizeof(c));
+	printf("Size of an int: %lu bytes(s)\n", (unsigned long)sizeof(i));
+	printf("Size of a long int: %lu bytes(s)\n", (unsigned long)sizeof(li));
+	printf("Size of a long long int: %lu bytes(s)\n", (unsigned long)sizeof(ill));
+	printf("Size of a float: %lu bytes(s)\n", (unsigned long)sizeof(f));
 }
